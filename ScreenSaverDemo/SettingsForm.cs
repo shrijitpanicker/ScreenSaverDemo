89b6657f36aa3a004e2a8cf89b4668dde26ad7be
shrijitpanicker <shrijit.panicker@cennest.com>
2022-02-23 19:15:56 +0530

    Fixed issues with settings form

--- ScreenSaverDemo/SettingsForm.cs
+++ ScreenSaverDemo/SettingsForm.cs
@@ -17,13 +17,11 @@ namespace ScreenSaverDemo
             LoadSettings();
         }
 
-        private bool IsSettingsFieldSet()
+        private float setValueInRegistryAndReturnValue(string key, string value)
         {
-            if (brightness != 0F && contrast != 0F && hue != 0F && saturation != 0F & gamma != 0F)
-            {
-                return true;
-            }
-            return false;
+            RegistryKey regKey = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Cennest_Demo_ScreenSaver");
+            regKey.SetValue(key, value);
+            return float.Parse(value);
         }
 
         private void LoadSettings()
@@ -40,34 +38,34 @@ namespace ScreenSaverDemo
             }
             else
             {
-                if (!IsSettingsFieldSet())
-                {
-                    brightness = (float.Parse((string)key.GetValue("brightness")));
-                    brightnessTrackBar.Value = ((int)brightness);
-                    contrast = (float.Parse((string)key.GetValue("contrast")));
-                    contrastTrackBar.Value = ((int)contrast);
-                    hue = (float.Parse((string)key.GetValue("hue")));
-                    hueTrackBar.Value = ((int)hue);
-                    saturation = (float.Parse((string)key.GetValue("saturation")));
-                    saturationTrackBar.Value = ((int)saturation);
-                    gamma = (float.Parse((string)key.GetValue("gamma")));
-                    gammaTrackBar.Value = ((int)gamma);
-                }
+                brightness = key.GetValue("brightness") != null ? (float.Parse((string)key.GetValue("brightness"))) : setValueInRegistryAndReturnValue("brightness", "10");
+                brightnessTrackBar.Value = ((int)brightness);
+                contrast = key.GetValue("contrast") != null ? (float.Parse((string)key.GetValue("contrast"))) : setValueInRegistryAndReturnValue("contrast", "10");
+                contrastTrackBar.Value = ((int)contrast);
+                hue = key.GetValue("hue") != null ? (float.Parse((string)key.GetValue("hue"))) : setValueInRegistryAndReturnValue("hue", "10"); ;
+                hueTrackBar.Value = ((int)hue);
+                saturation = key.GetValue("saturation") != null ? (float.Parse((string)key.GetValue("saturation"))) : setValueInRegistryAndReturnValue("saturation", "10"); ;
+                saturationTrackBar.Value = ((int)saturation);
+                gamma = key.GetValue("gamma") != null ? (float.Parse((string)key.GetValue("gamma"))) : setValueInRegistryAndReturnValue("gamma", "10"); ;
+                gammaTrackBar.Value = ((int)gamma);
             }
+
+            brightnessTextBox.Text = brightness.ToString();
+            contrastTextBox.Text = contrast.ToString();
+            hueTextBox.Text = hue.ToString();
+            saturationTextBox.Text = saturation.ToString();
+            gammaTextBox.Text = gamma.ToString();
         }
 
         private void SaveSettings()
         {
-            RegistryKey key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Demo_ScreenSaver");
-            // Create or get existing Registry subkey
-            if (IsSettingsFieldSet())
-            {
-                key.SetValue("brightness", brightness.ToString());
-                key.SetValue("contrast", contrast.ToString());
-                key.SetValue("hue", hue.ToString());
-                key.SetValue("saturation", saturation.ToString());
-                key.SetValue("gamma", gamma.ToString());
-            }
+            RegistryKey key = Registry.CurrentUser.CreateSubKey("SOFTWARE\\Cennest_Demo_ScreenSaver");
+            
+            key.SetValue("brightness", brightness.ToString());
+            key.SetValue("contrast", contrast.ToString());
+            key.SetValue("hue", hue.ToString());
+            key.SetValue("saturation", saturation.ToString());
+            key.SetValue("gamma", gamma.ToString());
         }
 
         private void okButton_Click(object sender, System.EventArgs e)
@@ -84,26 +82,31 @@ namespace ScreenSaverDemo
         private void brightnessTrackBar_Scroll(object sender, System.EventArgs e)
         {
             brightness = (float)(brightnessTrackBar.Value);
+            brightnessTextBox.Text = brightness.ToString();
         }
 
         private void contrastTrackBar_Scroll(object sender, System.EventArgs e)
         {
             contrast = (float)(contrastTrackBar.Value);
+            contrastTextBox.Text = contrast.ToString();
         }
 
         private void hueTrackBar_Scroll(object sender, System.EventArgs e)
         {
             hue = (float)(hueTrackBar.Value);
+            hueTextBox.Text = hue.ToString();
         }
 
         private void saturationTrackBar_Scroll(object sender, System.EventArgs e)
         {
             saturation = (float)(saturationTrackBar.Value);
+            saturationTextBox.Text = saturation.ToString();
         }
 
         private void gammaTrackBar_Scroll(object sender, System.EventArgs e)
         {
             gamma = (float)(gammaTrackBar.Value);
+            gammaTextBox.Text = gamma.ToString();
         }
     }
 }
