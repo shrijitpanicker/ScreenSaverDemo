ca194e974ceee0a3e3bc1df5be46f38c791c1058
shrijitpanicker <shrijit.panicker@cennest.com>
2022-02-23 19:55:05 +0530

    Changes to settings value initialization in settings form

--- ScreenSaverDemo/SettingsForm.cs
+++ ScreenSaverDemo/SettingsForm.cs
@@ -30,30 +30,35 @@ namespace ScreenSaverDemo
             // Get the value stored in the Registry
             if (key == null || key.SubKeyCount == 0 || key.SubKeyCount == 1)
             {
-                key.SetValue("brightness", "10");
-                key.SetValue("contrast", "10");
-                key.SetValue("hue", "10");
-                key.SetValue("saturation", "10");
-                key.SetValue("gamma", "10");
+                brightness = 10F;
+                key.SetValue("brightness", brightness.ToString());
+                contrast = 10F;
+                key.SetValue("contrast", contrast.ToString());
+                hue = 10F;
+                key.SetValue("hue", hue.ToString());
+                saturation = 10F;
+                key.SetValue("saturation", hue.ToString());
+                gamma = 10F;
+                key.SetValue("gamma", gamma.ToString());
             }
             else
             {
                 brightness = key.GetValue("brightness") != null ? (float.Parse((string)key.GetValue("brightness"))) : setValueInRegistryAndReturnValue("brightness", "10");
-                brightnessTrackBar.Value = ((int)brightness);
                 contrast = key.GetValue("contrast") != null ? (float.Parse((string)key.GetValue("contrast"))) : setValueInRegistryAndReturnValue("contrast", "10");
-                contrastTrackBar.Value = ((int)contrast);
-                hue = key.GetValue("hue") != null ? (float.Parse((string)key.GetValue("hue"))) : setValueInRegistryAndReturnValue("hue", "10"); ;
-                hueTrackBar.Value = ((int)hue);
-                saturation = key.GetValue("saturation") != null ? (float.Parse((string)key.GetValue("saturation"))) : setValueInRegistryAndReturnValue("saturation", "10"); ;
-                saturationTrackBar.Value = ((int)saturation);
-                gamma = key.GetValue("gamma") != null ? (float.Parse((string)key.GetValue("gamma"))) : setValueInRegistryAndReturnValue("gamma", "10"); ;
-                gammaTrackBar.Value = ((int)gamma);
+                hue = key.GetValue("hue") != null ? (float.Parse((string)key.GetValue("hue"))) : setValueInRegistryAndReturnValue("hue", "10");
+                saturation = key.GetValue("saturation") != null ? (float.Parse((string)key.GetValue("saturation"))) : setValueInRegistryAndReturnValue("saturation", "10");
+                gamma = key.GetValue("gamma") != null ? (float.Parse((string)key.GetValue("gamma"))) : setValueInRegistryAndReturnValue("gamma", "10");
             }
 
+            brightnessTrackBar.Value = ((int)brightness);
             brightnessTextBox.Text = brightness.ToString();
+            contrastTrackBar.Value = ((int)contrast);
             contrastTextBox.Text = contrast.ToString();
+            hueTrackBar.Value = ((int)hue);
             hueTextBox.Text = hue.ToString();
+            saturationTrackBar.Value = ((int)saturation);
             saturationTextBox.Text = saturation.ToString();
+            gammaTrackBar.Value = ((int)gamma);
             gammaTextBox.Text = gamma.ToString();
         }
 
